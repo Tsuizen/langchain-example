651c9576285a9c10e8d6b72b67daae3ac6c94fbd
Tsuzien <personal_rogers@outlook.com>
2023-06-08 00:23:00 +0800

    feat: 🎸 init project

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,27 @@
-import { Button, Header } from "ui";
+'use client';
+import { useEffect } from 'react';
+import { Button, Header } from 'ui';
+import { OpenAI } from 'langchain/llms/openai';
+
+const model = new OpenAI({
+  openAIApiKey: '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',
+  temperature: 0.9
+});
 
 export default function Page() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await model.call(
+        'What would be a good company name a company that makes colorful socks?'
+      );
+      
+    };
+    fetchData();
+  }, []);
+
   return (
     <>
-      <Header text="Web" />
+      <Header text='Web' />
       <Button />
     </>
   );
